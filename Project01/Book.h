b20d3d3c6b54467b00a88224ad52d02603a81f1b
ITSTEP\st <st>
2024-03-28 17:57:13 +0300

    2

--- Project01/Book.h
+++ Project01/Book.h
@@ -8,6 +8,23 @@ class Book
 public:
 	int num_pag;
 	string author;
-	string cover;
 	string name;
+
+	Book() {
+		num_pag = 100;
+		author = "no name";
+		name = "No name";
+	}
+
+	Book(int n_p,string ath,string nm) {
+		num_pag = n_p;
+		author = ath;
+		name = nm;
+	}
+
+	Book(string nm) {
+		num_pag = 0;
+		author = "";
+		name = nm;
+	}
 };
